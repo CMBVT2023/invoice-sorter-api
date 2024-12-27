2a906f0a47abbcab61c415a4e3b2e861b29726b8
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-12-26 16:40:40 -0800

    Added routing parameters to allow for reusability depending on the front end page the user is connecting from

--- app.js
+++ app.js
@@ -13,23 +13,35 @@ backEnd.use(cors({
     origin: 'http://localhost:5173/',
 }))
 
-backEnd.get('/getDirectories', async (req, res) => {
+backEnd.get('/:page/get-directories', async (req, res) => {
     try {
-        let customers = await fileAccess.getAllCustomerFolders();
-        res.send({customersArray: customers});
+        let { page: pageRouteQuery} = req.params;
+        let directories;
+
+        if (pageRouteQuery == 'customer-scanned-documents') {
+            directories = await fileAccess.getAllCustomerFolders();
+        }
+
+        res.send({directoriesArray: directories});
     } catch (error) {
         console.error(`Error: ${error}`);
         res.status(500).send('Server Error');
     }
 })
 
-backEnd.get('/getInvoice', async (req, res) => {
+backEnd.get('/:page/get-invoice', async (req, res) => {
     try {
-        let [ invoiceRelativePath, invoicePDF ] = await fileAccess.getInvoice();
+        let { page: pageRouteQuery} = req.params;
 
+        let invoiceRelativePath, invoicePDF;
+
+        if (pageRouteQuery == 'customer-scanned-documents') {
+            [ invoiceRelativePath, invoicePDF ] = await fileAccess.getInvoice();
+        }
+        
         //? A response body is used to store the relative file path and the file's encoded string before being sent to the user.
         let responseBody = {
-            fileName: invoiceRelativePath,
+        fileName: invoiceRelativePath,
             file: invoicePDF 
         }
 
@@ -40,11 +52,15 @@ backEnd.get('/getInvoice', async (req, res) => {
     }
 })
 
-backEnd.post('/sortFile', async (req, res) => {
+backEnd.post('/:page/sort-file', async (req, res) => {
     try {
+        let isSuccessful, transferMessage, undoObj;
         let requestQueryParameters = req.query;
+        let { page: pageRouteQuery} = req.params;
 
-        let [isSuccessful, transferMessage, undoObj] = await fileAccess.sortFile(requestQueryParameters);
+        if (pageRouteQuery == 'customer-scanned-documents') {
+            [isSuccessful, transferMessage, undoObj] = await fileAccess.sortFile(requestQueryParameters);
+        }
         let actionId = (Date.now() * Math.random()).toString(16)
         
         res.send({result: isSuccessful ? 'Succeeded' : 'Failed', message: transferMessage, undoInfo: undoObj, id: actionId, action: 'File Transfer'});
@@ -54,14 +70,16 @@ backEnd.post('/sortFile', async (req, res) => {
     }
 })
 
-backEnd.post('/createNewFolder', async (req, res) => {
+backEnd.post('/:page/create-new-folder', async (req, res) => {
     try {
         let isSuccessful, transferMessage, undoObj;
         let requestQueryParameters = req.query;
+        let { page: pageRouteQuery} = req.params;
 
-        if (requestQueryParameters.pageName == 'CUSTOMER_SCANNED_DOCUMENTS') {
+        if (pageRouteQuery == 'customer-scanned-documents') {
             [isSuccessful, transferMessage, undoObj] = await fileAccess.createNewFolder(requestQueryParameters);
         }
+
 
         let actionId = (Date.now() * Math.random()).toString(16)
 
@@ -72,12 +90,9 @@ backEnd.post('/createNewFolder', async (req, res) => {
     }
 })
 
-backEnd.post('/undoAction', async (req, res) => {
+backEnd.post('/undo-action', async (req, res) => {
     try {
         let requestQueryParameters = req.query;
-
-        // Remember to parse the undoInfo back into an object.
-
         
         let [isSuccessful, transferMessage, undoneActionId] = await fileAccess.undoPreviousAction(requestQueryParameters)
         let actionId = (Date.now() * Math.random()).toString(16)
